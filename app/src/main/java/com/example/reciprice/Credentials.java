83ea214bac9cf844966906d257762281335e1d83
CharlesLiu02 <31979415+CharlesLiu02@users.noreply.github.com>
2019-04-15 09:37:02 -0700

    Delete Credentials.java

--- app/src/main/java/com/example/reciprice/Credentials.java
+++
@@ -1,6 +0,0 @@
-package com.example.reciprice;
-
-public class Credentials {
-    public static final String APP_ID = "B00125E5-D43A-1BB7-FF6C-12C1FFD2FE00";
-    public static final String API_KEY = "E0459EFA-F04C-3AB5-FFCE-6B3F1212F000";
-}
